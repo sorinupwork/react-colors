986b66d18fbcc6135b880d341b0bf07b7fa9e762
Sorin Adam <sorinadam@Sorins-MacBook-Air.local>
2022-01-16 08:48:34 +0200

    make form and navbar responsive

--- src/styles/NavbarStyles.js
+++ src/styles/NavbarStyles.js
@@ -1,3 +1,4 @@
+import sizes from "./sizes";
 export default {
   Navbar: {
     display: "flex",
@@ -18,6 +19,9 @@ export default {
     "& a": {
       textDecoration: "none",
       color: "black",
+    },
+    [sizes.down("xs")]: {
+      display: "none",
     },
   },
 
@@ -42,6 +46,9 @@ export default {
         marginLeft: " -7px",
         marginTop: " -3px",
       },
+    [sizes.down("md")]: {
+      width: "160px",
+    },
   },
   selectContainer: {
     marginLeft: "auto",
